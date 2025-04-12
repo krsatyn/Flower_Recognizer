dfc806ff1940e7d6a9e1675e603a48f115c00f5c
kolyagin <92187267+krsatyn@users.noreply.github.com>
2025-04-12 23:37:10 +0400

    Dev собрал контейнер

--- app/utils.py
+++ app/utils.py
@@ -2,15 +2,13 @@ import numpy as np
 import os
 from sklearn.metrics.pairwise import cosine_similarity
 
-LIBRARY_DIR = r"app\image_library"
-EMBEDDING_FILE = r"app\model\embeddings\embeddings.npy"
+LIBRARY_DIR = os.path.join("app", "image_library")
+EMBEDDING_FILE = os.path.join("app", "model", "embeddings", "embeddings.npy")
 
 def load_library():
     
     all_image_paths = []                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      
     for folder in os.listdir(LIBRARY_DIR):
-        print('=====================')
-        print(folder)
         folder_path = os.path.join(LIBRARY_DIR, folder)
         if os.path.isdir(folder_path):
             all_image_paths += [os.path.join(folder_path, fname)
@@ -21,20 +19,13 @@ def load_library():
     # Эмбеддинги загружаем заранее
     embeddings = np.load(EMBEDDING_FILE)
     
-    print(f'{len(image_paths)=}')
-    print(f'{len(embeddings)=}')
+
     return image_paths, embeddings
  
 #  Функция поиска похожих изображений
 def find_top_k(query_emb, db_embs, db_paths, k=5):
     sim_scores = cosine_similarity([query_emb], db_embs)[0]
     top_indices = np.argsort(sim_scores)[::-1][:k]
-    
-    print('-------------------------------------')
-    print(len(db_paths))
-    print('-------------------------------------')
-    
-    print(top_indices)
     
     answer = {}
     for i in top_indices:
